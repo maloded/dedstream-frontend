5d620a2238b29f629f1baae0b8ed73efbefacbd6
Oleksii Malodied <damatomxxx@gmail.com>
2026-08-06 16:04:13 +0300

    fix: use remotePatterns for next/image

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,16 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./src/libs/i18n/request.ts');
 
 const nextConfig: NextConfig = {
-	reactStrictMode: true,
-	images: {
-		domains: ['dedstream.gmhost.space'], 
-	},
+    reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'dedstream.gmhost.space',
+                pathname: '/**',
+            },
+        ],
+    },
 };
 
 export default withNextIntl(nextConfig);
